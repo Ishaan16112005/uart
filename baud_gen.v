569b2ce6dda9401ec0662525cd652b49565c84b0
Ishaan16112005 <shettyishaan28@gmail.com>
2026-09-06 02:07:48 +0530

    Refactor baud_gen module for reset and tick logic
    Refactor baud_gen module to use active-low reset and correct tick generation logic.

--- baud_gen.v
+++ baud_gen.v
@@ -1,15 +1,30 @@
-module baud_gen(
+module baud_gen #(
+  parameter sys_clk = 1000000000,
+  parameter baud_rate = 115200,
+  parameter over_sample = 16
+) (
   input wire clk,
-  input wire reset,
+  input wire rst_n,
   output wire tick
 );
-  reg [8:0] counter_reg = 0;
-  assign tick = (counter_reg == 325);
-  always @(posedge clk or posedge reset) begin 
-    if (reset) begin
-      counter_reg <= 0;
-    end else if (tick) begin 
-      counter_reg <= 0;
-    else counter_reg <= counter_reg + 1;
+
+  localparam max_count = (sys_clk /(baud_rate * over_sample));
+
+  reg ($clog2(max_count) -1 : 0) counter;
+
+  always @(posedge clk or negedge rst_n) begin 
+    if(!rst_n) begin 
+      counter <= 0;
+      tick <= 1'b0;
+    end else begin 
+      if (counter <= (max_count -1)) begin 
+        counter <= 0;
+        tick <= 1'b1;
+      end else begin 
+        counter <= counter + 1;
+        tick <= 1'b0;
+      end
+    end
   end
+
 endmodule 
